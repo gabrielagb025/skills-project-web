3b1c22568e203f7fdcce862b1d715812cf13bbeb
Gabriela García <ggarciaborregon@gmail.com>
2023-10-07 13:42:36 +0200

    handle notifications

--- src/services/FriendRequestService.js
+++ src/services/FriendRequestService.js
@@ -5,3 +5,5 @@ const http = createHttp(true);
 export const sendFriendRequest = (id, body) => http.post(`/friend-request/${id}`, body);
 
 export const getFriendRequests = () => http.get('/friend-requests');
+
+export const respondToFriendRequest = (id, status) => http.patch(`/friend-request/edit/${id}`, { status });
--- src/views/currentUser/Notificacions/Notifications.jsx
+++ src/views/currentUser/Notificacions/Notifications.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getFriendRequests } from "../../../services/FriendRequestService";
+import { getFriendRequests, respondToFriendRequest } from "../../../services/FriendRequestService";
 import { NavLink } from "react-router-dom";
 
 const Notifications = () => {
@@ -15,12 +15,28 @@ const Notifications = () => {
         })
     }, [])
 
+    const handleRespondToFriendRequest = (id, action) => {
+        respondToFriendRequest(id, action)
+        .then((updatedRequest) => {
+            console.log('Solicitud editada')
+            const updatedRequests = friendRequestList.filter(request => request.id !== updatedRequest.id);
+                setFriendRequestList(updatedRequests);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
     return(
         <div className="Notifications container mt-4">
             <h1>Notificaciones</h1>
             <div className="friend-request-container">
                 <h3>Solicitudes de conexión</h3>
-                {friendRequestList.map((friendRequest) => (
+
+                {friendRequestList.length <= 0 ? 
+                (<p>No tienes ninguna solicitud de conexión</p>) 
+                : 
+                (friendRequestList.map((friendRequest) => (
                     <div className="mt-3" key={friendRequest.id}>
                     <div>
                     <NavLink to={`/user/users/detail/${friendRequest.userSend.id}`}><img className="mb-3" src={friendRequest.userSend.avatar} alt="" width={100}/></NavLink>
@@ -28,12 +44,12 @@ const Notifications = () => {
                     <p>{friendRequest.message}</p>
                     </div>
                     <div className="friend-request-buttons d-flex">
-                        <button className="btn btn-success me-3">Aceptar</button>
-                        <button className="btn btn-danger">Rechazar</button>
+                        <button className="btn btn-success me-3" onClick={() => handleRespondToFriendRequest(friendRequest.id, 'accepted')}>Aceptar</button>
+                        <button className="btn btn-danger" onClick={() => handleRespondToFriendRequest(friendRequest.id, 'rejected')}>Rechazar</button>
                     </div>
                     <hr />
                     </div>
-                ))}
+                )))}
             </div>
         </div>
     )
