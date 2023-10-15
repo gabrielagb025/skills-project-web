382f2042da831a9d7b319a87f3b0b86de363584f
Gabriela García <ggarciaborregon@gmail.com>
2023-10-15 21:05:08 +0200

    home view

--- src/views/Auth/Home/Home.jsx
+++ src/views/Auth/Home/Home.jsx
@@ -10,13 +10,20 @@ const Home = () => {
         user ? (
             <Navigate to="/user/profile" />
         ) : (
-            <div className="Home home-container container mt-5">
-                <h1 className="mt-5">Bienvenido a <br/><span className="fw-bold">SkillSync.</span></h1>
-                <p className="mt-3 text-center">Conecta con otros usuarios, <br/> comparte tus conocimientos <br/> y adquiere nuevas perspectivas.</p>
+            <div className="Home home-container container">
+                <img src="src\assets\logo skillsync letras.png" alt="" width={200} />
+                <div className="row justify-content-around align-items-center">
+                    <div className="col-md-6 text-cetner">
+                        <h2 className="ms-3 mt-3">Bienvenido a <br /><span className="fw-bold">SkillSync.</span></h2>
+                        <img className="mt-2 me-2 img-fluid d-none d-md-block" src="src\assets\imagen skillsync-01.png" alt="" width={600} />
+                        <img className="mt-2 me-2 img-fluid d-md-none" src="src\assets\imagen skillsync-01.png" alt="" width={400} />
+                    </div>
 
-                <div className="home-buttons mt-4">
-                    <NavLink className="" to="/register"><button className="mt-3 btn register-btn">Registrarse</button></NavLink>
-                    <p className="mt-4">¿Ya tienes una cuenta? - <NavLink to="/login" style={{ textDecoration: 'none' }}><span>Iniciar sesión</span></NavLink></p>
+                    <div className="col-md-6 mt-4 home-buttons text-center">
+                        <p className="text-center">Conecta con otros usuarios, <br /> comparte tus conocimientos <br /> y adquiere nuevas perspectivas.</p>
+                        <NavLink className="" to="/register"><button className="mt-3 btn register-btn">Registrarse</button></NavLink>
+                        <p className="mt-4">¿Ya tienes una cuenta? - <NavLink to="/login" style={{ textDecoration: 'none' }}><span>Iniciar sesión</span></NavLink></p>
+                    </div>
                 </div>
             </div>
         )
