071fc8cd7e696fb24d63bac3bfe4590a0d6fd001
Gabriela García <ggarciaborregon@gmail.com>
2023-10-16 13:08:38 +0200

    user card styles

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -3,28 +3,30 @@ import { Link } from 'react-router-dom';
 
 const UserCard = (user) => {
     return (
-        <div className="card mt-4" style={{ width: '100%' }}>
-            <div className="row no-gutters">
-                <div className="col-md-4 d-flex align-items-center">
+        <div className="card card-user mt-4" style={{ width: '100%' }}>
+            <div className="info-user-card row no-gutters">
+                <div className="photo-name-container">
                     <div className="card-img-container">
                         <img src={user.avatar} className="user-img" alt="..."/>
                     </div>
                     <div className="ms-3">
                         <h3 className="card-title">{user.name}</h3>
-                        <p className="card-text">{user.city}</p>
+                        <p className="card-text"><i className="bi bi-geo-alt-fill"></i>{user.city}</p>
                     </div>
                 </div>
                 <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-text">{user.name} puede enseñar:</h5>
+                    <div className="mt-2 skills-user-card card-body">
+                        <h5 className="card-text"><i class="bi bi-pen-fill"></i>{user.name} puede enseñar:</h5>
                         {user.teachSkills.map((skill) => (
-                            <p key={skill.id}>{skill.name}</p>
+                            <p key={skill.id}>- {skill.name}</p>
                         ))}
-                        <h5 className="card-text">{user.name} quiere aprender:</h5>
+                        <h5 className="card-text mt-4"><i class="bi bi-pen-fill"></i>{user.name} quiere aprender:</h5>
                         {user.learnSkills.map((skill) => (
-                            <p key={skill.id}>{skill.name}</p>
+                            <p key={skill.id}>- {skill.name}</p>
                         ))}
-                        <Link to={`/user/users/detail/${user.id}`}><button className="btn btn-primary">Ver más</button></Link>
+                        <div className="mt-4 submit-button">
+                        <Link to={`/user/users/detail/${user.id}`}><button className="btn">Ver perfil</button></Link>
+                        </div>
                     </div>
                 </div>
             </div>
